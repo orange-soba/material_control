c4bc34230567074f48d75346ce6f92188e093030
orange-soba <t.sato.terusa6k@gmail.com>
2023-10-30 15:14:47 +0900

    コントローラーの初期設定

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,2 +1,14 @@
 class PartsController < ApplicationController
+  before_action :redirect_to_root
+
+  def new
+  end
+  
+  private
+
+  def redirect_to_root
+    return if user_signed_in?
+
+    redirect_to root_path
+  end
 end
