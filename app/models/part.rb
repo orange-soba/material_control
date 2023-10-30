3bf0271cdddc1070f17249decf2ecd3382aba879
orange-soba <t.sato.terusa6k@gmail.com>
2023-10-30 17:49:36 +0900

    バリデーションの修正

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,6 +2,7 @@ class Part < ApplicationRecord
   belongs_to :user
   
   with_options presence: true do
-    validates :name, :stock, :finished
+    validates :name, :stock
   end
+  validates :finished, inclusion: { in: [true, false] }
 end
