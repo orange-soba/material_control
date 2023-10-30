4f3b25b906867e0cb658850f9744b018c1a4b963
orange-soba <t.sato.terusa6k@gmail.com>
2023-10-30 17:42:53 +0900

    コントローラーの更新

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -2,9 +2,24 @@ class PartsController < ApplicationController
   before_action :redirect_to_root
 
   def new
+    @part = Part.new
+  end
+
+  def create
+    @part = Part.new(part_params)
+    if @part.valid?
+      @part.save
+      redirect_to root_path # 本当はマイページ
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
   private
+
+  def part_params
+    params.require(:part).permit(:name, :stock, :finished).merge(user_id: current_user.id)
+  end
 
   def redirect_to_root
     return if user_signed_in?
