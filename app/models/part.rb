fc30297dec0b216f3c36715007110520840c64e2
orange-soba <t.sato.terusa6k@gmail.com>
2023-10-31 14:06:19 +0900

    バリデーションの修正

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,7 +2,8 @@ class Part < ApplicationRecord
   belongs_to :user
   
   with_options presence: true do
-    validates :name, :stock
+    validates :name
+    validates :stock, numericality: { only_integere: true, allow_nil: true, message: 'は半角の数値で入力してください' }
   end
   validates :finished, inclusion: { in: [true, false] }
 end
