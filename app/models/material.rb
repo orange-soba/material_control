f9f9779f83221eb7ee8e71663d10017e74578271
orange-soba <t.sato.terusa6k@gmail.com>
2023-11-01 17:44:32 +0900

    バリデーション

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,2 +1,7 @@
 class Material < ApplicationRecord
+  belongs_to :user
+  
+  with_options presence: true do
+    validates :material_type, :category, :thickness, :length, :stock, :material_id
+  end
 end
