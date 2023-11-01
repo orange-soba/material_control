293451aaf1c90943f796dc3a39b49d58df0e34d7
orange-soba <t.sato.terusa6k@gmail.com>
2023-11-01 14:54:23 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root "static_pages#home"
   resources :users, only: :show
   resource :parts, only: [:new, :create]
+  resources :materials, only: [:new, :create]
 end
