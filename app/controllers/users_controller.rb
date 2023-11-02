5f22ae488774641c9bca29a6260e45ac6849d8f8
orange-soba <t.sato.terusa6k@gmail.com>
2023-11-02 21:52:16 +0900

    usersコントローラーの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     @products = current_user.parts.where(finished: true)
     @parts = current_user.parts.where(finished: false)
