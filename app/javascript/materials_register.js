8721ccc882e718cac80052d44b4fae62a79aa916
orange-soba <t.sato.terusa6k@gmail.com>
2023-11-02 13:58:58 +0900

    javascriptの修正

--- app/javascript/materials_register.js
+++ app/javascript/materials_register.js
@@ -1,5 +1,3 @@
-import { formatDate, show_error_messages, remove_children } from "./export_function";
-
 function materials_register() {
   const form = document.getElementById('materials-register-form');
   if (!form) return null;
@@ -82,6 +80,36 @@ function create_html(material) {
 
   return html;
 };
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+};
+
+function show_error_messages(messages) {
+  const area = document.getElementById('error-messages-area');
+  const lists = document.getElementById('error-messages-lists');
+
+  area.removeAttribute('style', 'display:none;');
+  remove_children(lists)
+  messages.forEach((message) => {
+    const html = `<li class='error-message'>!${message}!</li>`
+    lists.insertAdjacentHTML('beforeend', html);
+  });
+};
+
+function remove_children(lists) {
+  while(lists.lastChild) {
+    lists.removeChild(lists.lastChild);
+  }
+};
+
 
 window.addEventListener('turbo:load', materials_register);
 window.addEventListener('turbo: render', materials_register);
