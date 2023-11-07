9552d7afd8fce5199bbe53cb7da22ed32728de1f
orange-soba <t.sato.terusa6k@gmail.com>
2023-11-07 22:57:09 +0900

    ビューファイルの更新

--- app/controllers/need_materials_controller.rb
+++ app/controllers/need_materials_controller.rb
@@ -1,5 +1,7 @@
 class NeedMaterialsController < ApplicationController
   def new
     @part = Part.find(params[:part_id])
+    @need_material = NeedMaterial.new
+    @materials = Material.where(user_id: current_user.id)
   end
 end
