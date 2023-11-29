e367bda6f5e37601b202d77bfaa531ce3f8e4952
orange-soba <t.sato.terusa6k@gmail.com>
2023-11-29 16:32:12 +0900

    コントローラーの更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字（6文字以上）での入力が必須です' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字（6文字以上）での入力が必須です', if: :password_required? }
 
   has_many :parts
   has_many :materials
