2ed734a330634cd96fe1167d07f344e6b58219a8
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-04 18:28:34 +0900

    FactoryBotの更新

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    name                  { Faker::Company.name }
+    password              { Faker::Lorem.characters(number: 10, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
   end
 end
