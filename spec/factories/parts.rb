7d92515bd64704f5047861eae12c115b74539bef
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-06 19:32:30 +0900

    part単体テストコードの雛形の作成

--- spec/factories/parts.rb
+++ spec/factories/parts.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :part do
     name { Faker::Commerce.product_name }
     stock { Faker::Number.number(digits: 4) }
-    finished { rand(2) == 1 }
+    finished { false }
 
     association :user
   end
--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Part, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '完成品/部品新規登録' do
+    before do
+      @part = FactoryBot.build(:part)
+    end
+    context '新規登録できる場合' do
+      it '必要な情報が揃っていれば登録できる(finishedがfalse:部品)' do
+      end
+      it 'finishedがtrue(完成品)でも登録できる' do
+      end
+    end
+    context '新規登録できない場合' do
+      it 'nameが空白だと登録できない' do
+      end
+      it 'stockが空白だと登録できない' do
+      end
+      it 'finishedが空白だと登録できない' do
+      end
+      it '同じユーザーが同じnameのpartを登録できない' do
+      end
+      it 'stockは文字列だと登録できない' do
+      end
+      it 'stockは全角の数字だと登録できない' do
+      end
+      it 'userに紐づいていないと登録できない' do
+      end
+    end
+  end
 end
