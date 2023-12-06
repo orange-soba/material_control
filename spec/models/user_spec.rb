266f9befed3ac3c629f65fa23ea1b8e8e881263d
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-06 19:06:20 +0900

    ユーザー単体テストの実施

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,26 +14,67 @@ RSpec.describe User, type: :model do
 
     context '新規登録できない場合' do
       it 'nameが空だと登録できない' do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('名前を入力してください')
       end
       it 'emailが空だと登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Eメールを入力してください')
       end
       it 'すでに登録済みのemailだと登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
       end
       it 'emailに@が含まれていないと登録できない' do
+        @user.email = 'test.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Eメールは不正な値です')
       end
       it 'passwordが空だと登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードを入力してください')
+        expect(@user.errors.full_messages).to include('パスワードは半角英数字（6文字以上）での入力が必須です')
       end
       it 'password_confirmationが空だと登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワード（確認用）とパスワードの入力が一致しません')
       end
       it 'passwordとpassword_confirmationが一致しないと登録できない' do
+        @user.password = 'password1'
+        @user.password_confirmation = 'passw0rd1'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワード（確認用）とパスワードの入力が一致しません')
       end
       it 'passwordが6文字以上でないと登録できない' do
+        @user.password = 'pass1'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
       end
       it 'passwordが英字のみだと登録できない' do
+        @user.password = 'password'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードは半角英数字（6文字以上）での入力が必須です')
       end
       it 'passwordが数字のみだと登録できない' do
+        @user.password = '1234567890'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードは半角英数字（6文字以上）での入力が必須です')
       end
       it 'passwordに英数字以外の文字が含まれていると登録できない' do
+        @user.password = 'password1パス'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードは半角英数字（6文字以上）での入力が必須です')
       end
     end
   end
