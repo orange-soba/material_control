8e24c16a6d05a0f7646cb94d44aba2c5c65f6292
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-07 15:38:14 +0900

    partが新規登録できない場合

--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -16,18 +16,42 @@ RSpec.describe Part, type: :model do
     end
     context '新規登録できない場合' do
       it 'nameが空白だと登録できない' do
+        @part.name = ''
+        @part.valid?
+        expect(@part.errors.full_messages).to include('完成品名/部品名を入力してください')
       end
       it 'stockが空白だと登録できない' do
+        @part.stock = ''
+        @part.valid?
+        expect(@part.errors.full_messages).to include('在庫数を入力してください')
       end
       it 'finishedが空白だと登録できない' do
+        @part.finished = nil
+        @part.valid?
+        expect(@part.errors.full_messages).to include('完成品か否かは一覧にありません')
       end
       it '同じユーザーが同じnameのpartを登録できない' do
+        @part.save
+        another_part = FactoryBot.build(:part)
+        another_part.user = @part.user
+        another_part.name = @part.name
+        another_part.valid?
+        expect(another_part.errors.full_messages).to include('完成品名/部品名はすでに存在します')
       end
       it 'stockは文字列だと登録できない' do
+        @part.stock = 'ゼロ'
+        @part.valid?
+        expect(@part.errors.full_messages).to include('在庫数は半角の数値で入力してください')
       end
       it 'stockは全角の数字だと登録できない' do
+        @part.stock = '０'
+        @part.valid?
+        expect(@part.errors.full_messages).to include('在庫数は半角の数値で入力してください')
       end
       it 'userに紐づいていないと登録できない' do
+        @part.user = nil
+        @part.valid?
+        expect(@part.errors.full_messages).to include('ユーザーを入力してください')
       end
     end
   end
