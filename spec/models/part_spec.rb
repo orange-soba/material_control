01b3f8fee729bc977bf4becf81babe7385bd52cd
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-07 15:27:11 +0900

    新規登録できる場合

--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -7,8 +7,11 @@ RSpec.describe Part, type: :model do
     end
     context '新規登録できる場合' do
       it '必要な情報が揃っていれば登録できる(finishedがfalse:部品)' do
+        expect(@part).to be_valid
       end
       it 'finishedがtrue(完成品)でも登録できる' do
+        @part.finished = true
+        expect(@part).to be_valid
       end
     end
     context '新規登録できない場合' do
