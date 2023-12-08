b1a3627ba54578651de513f596f7f11a158c77c0
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-08 17:04:44 +0900

    parts_relation単体テストの雛形の作成

--- spec/models/parts_relation_spec.rb
+++ spec/models/parts_relation_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe PartsRelation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '必要部品登録' do
+    context '必要部品登録できる場合' do
+      it '必要な条件が揃っていて、親が完成品、子が部品は登録できる' do
+      end
+      it '必要な条件が揃っていて、親が部品、子が部品は登録できる' do
+      end
+    end
+    context '必要部品登録できない場合' do
+      it '完成品は子になれない' do
+      end
+      it 'necessary_numsが空白では登録できない' do
+      end
+      it 'necessary_numsは文字列では登録できない' do
+      end
+      it 'necessary_numsは0以下の数値では登録できない' do
+      end
+      it '子の子に親がいる場合は登録できない' do
+      end
+      it '親の親に子がいる場合は登録できない' do
+      end
+    end
+  end
 end
