8a858a2707de5140fa0c34dbcbd1df47f5fbde3f
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-09 20:31:53 +0900

    need_materialのFactoryBotの更新

--- spec/factories/need_materials.rb
+++ spec/factories/need_materials.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :need_material do
-    
+    length         { Faker::Number.within(range: 1..4000) }
+    length_option  { Faker::Lorem.word }
+    necessary_nums { Faker::Number.within(range: 1..100) }
   end
 end
