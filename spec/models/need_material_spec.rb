7c472d9aa35c586b6006913cda526e1dadae271f
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-09 21:17:05 +0900

    need_material単体テストの登録出来ない場合の作成

--- spec/models/need_material_spec.rb
+++ spec/models/need_material_spec.rb
@@ -17,22 +17,40 @@ RSpec.describe NeedMaterial, type: :model do
     end
     context '新規登録できない場合' do
       it 'partが空だと登録できない' do
+        @need_material.part = nil
+        valid_check(@need_material, 'Partを入力してください', true)
       end
       it 'material_idが空白だと登録できない' do
+        @need_material.material_id = ''
+        valid_check(@need_material, '作成に必要な材料を入力してください', true)
       end
       it 'lengthが空白だと登録できない' do
+        @need_material.length = ''
+        valid_check(@need_material, '必要な長さを入力してください', true)
       end
       it 'lengthが文字列だと登録できない' do
+        @need_material.length = '１'
+        valid_check(@need_material, '必要な長さは数値で入力してください', true)
       end
       it 'lengthが0以下だと登録できない' do
+        @need_material.length = 0
+        valid_check(@need_material, '必要な長さは0より大きい値にしてください', true)
       end
       it 'necessary_numsが空白だと登録できない' do
+        @need_material.necessary_nums = ''
+        valid_check(@need_material, '必要数を入力してください', true)
       end
       it 'necessary_numsが文字列だと登録できない' do
+        @need_material.necessary_nums = '１'
+        valid_check(@need_material, '必要数は数値で入力してください', true)
       end
       it 'necessary_numsが0以下だと登録できない' do
+        @need_material.necessary_nums = 0
+        valid_check(@need_material, '必要数は0より大きい値にしてください', true)
       end
       it 'userが空だと登録出来ない' do
+        @need_material.user = nil
+        valid_check(@need_material, 'Userを入力してください', true)
       end
     end
   end
