fe9d2bab15a3a78b5a17b3e5515ea5063b02ce46
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-14 14:55:28 +0900

    ログインするためのサポートモジュールの作成

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,7 @@ end
 RSpec.configure do |config|
   config.include ValidCheck
   config.include SignInBasic
+  config.include SignIn
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
