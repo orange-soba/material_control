d7e7101a6c20678bf711d0cf829cb7e356426255
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-21 19:41:38 +0900

    必要部品登録に関するparts結合テストの更新

--- spec/system/parts_spec.rb
+++ spec/system/parts_spec.rb
@@ -343,12 +343,31 @@ RSpec.describe '必要部品の登録', type: :system do
     end
     it 'A=>Bという登録があるときにまたA=>Bという登録はできない' do
       # @parentに@childを必要部品として登録する
+      PartsRelation.create(parent_id: @parent.id, child_id: @child.id, necessary_nums: rand(10), user_id: @user.id)
       # ログイン
-      # 「部品」をクリックして折りたたみ要素を開く
+      sign_in(@user)
+      # 「完成品」をクリックして折りたたみ要素を開く
+      find('details.products-details').find('summary').click
+      sleep 1
       # 登録済みの@parentの名前をクリックして詳細ページへ遷移する
+      find_link(@parent.name).click
+      sleep 1
       # 「部品登録」ボタンをクリックし、必要部品登録ページへ遷移するのを確認
+      href = 'a[href="/parts/' + @parent.id.to_s + '/parts_relations/new"]'
+      find(href).click
+      sleep 1
+
+      expect(current_path).to eq new_part_parts_relations_path(@parent)
       # セレクトボックスから@childの名前を選択し、必要数を入力する
+      option = 'option[value="' + @child.id.to_s + '"]'
+      num = rand(10)
+      find('select.parts-relation__input').find(option).select_option
+      fill_in '　　　必要数：', with: num
       # 「登録」ボタンをクリックしても、Parts_relationモデルのカウントが変化しないのを確認
+      expect{
+        click_on '登録'
+        sleep 1
+      }.to change { PartsRelation.count }.by(0)
     end
   end
 end
