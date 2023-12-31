a7d5bb2d6f9bd522f2f4edbb337a49e6edc90ccd
orange-soba <t.sato.terusa6k@gmail.com>
2023-12-31 21:51:41 +0900

    新規登録に関するmaterials結合テストの更新

--- spec/system/materials_spec.rb
+++ spec/system/materials_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Materials", type: :system do
+RSpec.describe "材料の新規登録", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @material = FactoryBot.build(:material)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context '材料の新規登録ができる場合' do
+    it '正しい情報を入力すれば材料の新規登録ができ、登録履歴に入力した情報が表示される' do
+    end
+  end
+  context '材料の新規登録ができない場合' do
+    it 'ログインしていないと材料の新規ページへ遷移できない' do
+    end
+    it '誤った情報を入力すると新規登録ができず、新規登録ページへ戻ってくる' do
+    end
+  end
 end
