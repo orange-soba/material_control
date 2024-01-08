dabfbf5066eed0aeb8b6be3a42e3655fd960cb3c
orange-soba <t.sato.terusa6k@gmail.com>
2024-01-08 17:04:44 +0900

    必要材料の編集に関するmaterials結合テストの更新

--- spec/system/materials_spec.rb
+++ spec/system/materials_spec.rb
@@ -452,12 +452,35 @@ RSpec.describe '必要材料の編集', type: :system do
   context '必要材料の編集ができない場合' do
     it '部品詳細ページで誤った情報を入力すると必要材料の編集ができない' do
       # ログイン
+      sign_in(@user)
       # 「部品」をクリックして折りたたみ要素を開く
+      find('details.parts-details').find('summary').click
+      sleep 1
       # 登録済みの@partの名前をクリックして詳細ページへ遷移する
+      find_link(@part.name).click
+      sleep 1
       # 登録済みの必要材料が表示されているのを確認
+      expect(page).to have_css('div.need-materials') do |div|
+        expect(div).to have_content(@material.display_combine)
+        length = find_field('need_material_length').value.to_f.round(2)
+        expect(length).to eq(@need_material.length.round(2))
+        length_option = find_field('need_material_length_option').value.to_f.round(2)
+        expect(length_option).to eq(@need_material.length_option.round(2))
+        expect(div).to have_field('need_material_necessary_nums', with: @need_material.necessary_nums)
+      end
       # 誤った情報を入力
+      new_length = 0
+      fill_in 'need_material_length', with: new_length
       # 「更新」ボタンをクリック
+      within '.need-material__input-length' do
+        click_on '更新'
+      end
+      sleep 1
       # 表示されている情報が変化のないのを確認
+      expect(page).to have_css('div.need-materials') do |div|
+        expect(div).to have_field('need_material_length', with: @need_material.length.round(2))
+        expect(div).not_to have_field('need_material_length', with: new_length)
+      end
     end
   end
 end
