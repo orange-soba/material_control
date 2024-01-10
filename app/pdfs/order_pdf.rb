e769f229ced262de0b394609a6cee44b30ca0399
orange-soba <t.sato.terusa6k@gmail.com>
2024-01-10 17:33:22 +0900

    PDF用のクラスの作成

--- app/pdfs/order_pdf.rb
+++ app/pdfs/order_pdf.rb
@@ -1,3 +1,13 @@
 class OrderPdf < Prawn::Document
-  
+  def initialize
+    super(
+      page_size: 'A4',
+      top_margin: 40,
+      bottom_margin: 30,
+      left_margin: 20,
+      right_margin: 20
+    )
+
+    font 'app/assets/fonts/ipaexg.ttf'
+  end
 end
