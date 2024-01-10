8bf5d726e150a7474d857ec5fcca145088f14667
orange-soba <t.sato.terusa6k@gmail.com>
2024-01-10 20:04:12 +0900

    コントローラーの更新

--- app/controllers/order_pdfs_controller.rb
+++ app/controllers/order_pdfs_controller.rb
@@ -1,2 +1,13 @@
 class OrderPdfsController < ApplicationController
+  def index
+    respond_to do |format|
+      format.pdf do
+        order_pdf = OrderPdf.new().render
+        send_data order_pdf,
+          filename: 'order.pdf',
+          type: 'application/pdf',
+          disposition: 'inline'
+      end
+    end
+  end
 end
