4cc663cfa2bc4d5f0508f9ad7f91a4d87bc635b0
orange-soba <t.sato.terusa6k@gmail.com>
2024-01-16 15:27:44 +0900

    発注内容の動的作成のデモ

--- app/pdfs/order_pdf.rb
+++ app/pdfs/order_pdf.rb
@@ -42,15 +42,19 @@ class OrderPdf < Prawn::Document
     # 発注内容
     move_cursor_to 600
     order_details = [['発注内容', '個数(ヶ)', 'その他']]
-    (0..17).each do
-      order_details << ['', '', '']
+    (0..17).each do |index|
+      if index < parts_order.length
+        order_details << [parts_order[index][0], parts_order[index][1], '']
+      else
+        order_details << ['', '', '']
+      end
     end
-    demo_data = [['モーター', 2, ''], ['減速機', 1, ''], ['#35 防錆油 チェーン 2m', 5, '']]
-    demo_data.each_with_index do |data, index|
-      order_details[index + 1][0] = data[0]
-      order_details[index + 1][1] = data[1]
-      order_details[index + 1][2] = data[2]
-    end
+    # demo_data = [['モーター', 2, ''], ['減速機', 1, ''], ['#35 防錆油 チェーン 2m', 5, '']]
+    # demo_data.each_with_index do |data, index|
+    #   order_details[index + 1][0] = data[0]
+    #   order_details[index + 1][1] = data[1]
+    #   order_details[index + 1][2] = data[2]
+    # end
     table order_details, cell_style: { height: 30 }, column_widths: [400, 60, 60] do
       row(0).align = :center
       columns(1).align = :center
