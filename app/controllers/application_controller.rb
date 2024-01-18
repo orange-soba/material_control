c023a2792d61546c269d8164a348d95ede166233
orange-soba <t.sato.terusa6k@gmail.com>
2024-01-18 20:24:24 +0900

    deviseで許可するパラメーターの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :post_code, :prefecture_id, :city, :house_number, :building, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :post_code, :prefecture_id, :city, :house_number, :building, :phone_number])
   end
 end
