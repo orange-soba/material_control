5476dc7bd56a7b2ce4e53d4e0c1c71bc059236f1
orange-soba <t.sato.terusa6k@gmail.com>
2024-01-22 19:34:16 +0900

    FactoryBotの更新とその他修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,11 @@ FactoryBot.define do
     email                 { Faker::Internet.email }
     password              { Faker::Lorem.characters(number: 10, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
+    post_code             { '123-4567' }
+    prefecture_id         { 1 }
+    city                  { '蒲郡市一町どこそこ' }
+    house_number          { '1-1' }
+    building              { '大きなビル' }
+    phone_number          { '0123-456-789' }
   end
 end
