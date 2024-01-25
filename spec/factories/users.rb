dc6075be1b15363d8d1e17500d72410cab88e681
orange-soba <t.sato.terusa6k@gmail.com>
2024-01-25 16:29:11 +0900

    FactoryBotの更新

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,11 +4,11 @@ FactoryBot.define do
     email                 { Faker::Internet.email }
     password              { Faker::Lorem.characters(number: 10, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
-    post_code             { '123-4567' }
-    prefecture_id         { 1 }
-    city                  { '蒲郡市一町どこそこ' }
-    house_number          { '1-1' }
-    building              { '大きなビル' }
-    phone_number          { '0123-456-789' }
+    post_code             { rand(100..999).to_s + '-' + rand(1000..9999).to_s }
+    prefecture_id         { rand(1..47) }
+    city                  { Gimei.city.kanji + Gimei.town.kanji }
+    house_number          { rand(1..999).to_s + '-' + rand(1..9999).to_s }
+    building              { Faker::Lorem.word }
+    phone_number          { Faker::PhoneNumber.cell_phone }
   end
 end
