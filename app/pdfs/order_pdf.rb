85afb3b0c49966fead02509e36a7be627c01e098
orange-soba <t.sato.terusa6k@gmail.com>
2024-02-05 21:25:15 +0900

    コンポーネントの作成

--- app/pdfs/order_pdf.rb
+++ app/pdfs/order_pdf.rb
@@ -13,21 +13,46 @@ class OrderPdf < Prawn::Document
     #-------- 以下出力文章 ----------
 
     # タイトル
-    text '発注書', size: 20, align: :center
+    title
+
     move_down 20
 
     # ページ数
-    draw_text '1/1', at: [500, 760]
+    count_page(1, 1)
 
     # 発注先
+    order_destinate(order_destination)
+
+    # 発注元
+    order_source(user)
+
+    move_cursor_to 600
+
+    # 発注内容
+    order_contents(parts_order)
+
+    # 発注日
+    order_date
+  end
+
+  def title
+    text '発注書', size: 20, align: :center
+  end
+
+  def count_page(now, sum)
+    draw_text "#{now}/#{sum}", at: [500, 760]
+  end
+
+  def order_destinate(order_destination)
     order_to = [[order_destination, '御中']]
     table order_to, column_widths: [150, 35] do
       cells.borders = [:bottom]
       cells.height = 30
       columns(1).size = 10
     end
+  end
 
-    # 発注元
+  def order_source(user)
     bounding_box([320, 720], width: 200) do
       rounded_rectangle([0, cursor], 200, 110, 5) 
       text_box '発注元: ', at: [5, cursor - 5], size: 10
@@ -37,12 +62,19 @@ class OrderPdf < Prawn::Document
       stroke do
         line [5, 0], [100, 0]
       end
-      order_from_address = [[user.post_code], [user.prefecture.name + user.city + user.house_number], [user.building], ["Tell: #{user.phone_number}"]]
-      table order_from_address, cell_style: { borders: [], size: 10, height: 19 }
+      order_address = [[user.post_code], [user.prefecture.name + user.city + user.house_number], [user.building], ["Tell: #{user.phone_number}"]]
+      table order_address, cell_style: { borders: [], size: 10, height: 19 }
     end
+  end
 
-    # 発注内容
-    move_cursor_to 600
+  def order_date
+    today = Date.today.strftime('%Y/%m/%d')
+    bounding_box([0, 620], width: 130) do
+      text "発注日: #{today}"
+    end
+  end
+
+  def order_contents(parts_order)
     order_details = [['発注内容', '個数(ヶ)', 'その他']]
     (0..17).each do |index|
       if index < parts_order.length
@@ -68,12 +100,6 @@ class OrderPdf < Prawn::Document
       columns(0).border_right_width = 2
       columns(1).border_right_width = 2
       columns(2).border_right_width  =2
-    end
-
-    # 発注日
-    today = Date.today.strftime('%Y/%m/%d')
-    bounding_box([0, 620], width: 130) do
-      text "発注日: #{today}"
     end
   end
 end
